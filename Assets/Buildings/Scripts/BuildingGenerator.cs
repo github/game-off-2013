ea368ec49875e715ac3c8263fbc2ec502d71aeb7
Hylinn Taggart <hylinn.taggart+git@gmail.com>
2013-11-09 16:50:25 -0500

    Initial Scene

---
+++ Assets/Buildings/Scripts/BuildingGenerator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingGenerator : MonoBehaviour {
+
+	void Awake () {
+		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.localScale = new Vector3(10.0f, 6.096f, 10.0f);
+		cube.transform.localPosition = new Vector3((float) Random.Range(-100,100), 3.048f, (float) Random.Range (-100,100));
+		//cube.transform.position = new Vector3(0,0,0);
+	}
+	
+}
---
+++ Assets/Camera/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	
+	void Start() {
+		offset = transform.position;	
+	}
+	
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;	
+	}
+}
