ff255262132af47da567d42579eb0a789c728adb
Antti Kolehmainen <antionio@gmail.com>
2013-11-20 00:13:17 +0200

    Serious stuff

--- roomforchange/roomforchange/src/com/sturdyhelmetgames/roomforchange/screen/GameScreen.java
+++ roomforchange/roomforchange/src/com/sturdyhelmetgames/roomforchange/screen/GameScreen.java
@@ -137,18 +137,18 @@ public class GameScreen extends Basic2DScreen {
 
 	@Override
 	public boolean keyDown(int keycode) {
-		if (keycode == Keys.W) {
-			startScreenQuake(Level.UP);
-		}
-		if (keycode == Keys.S) {
-			startScreenQuake(Level.DOWN);
-		}
-		if (keycode == Keys.A) {
-			startScreenQuake(Level.LEFT);
-		}
-		if (keycode == Keys.D) {
-			startScreenQuake(Level.RIGHT);
-		}
+		// if (keycode == Keys.W) {
+		// startScreenQuake(Level.UP);
+		// }
+		// if (keycode == Keys.S) {
+		// startScreenQuake(Level.DOWN);
+		// }
+		// if (keycode == Keys.A) {
+		// startScreenQuake(Level.LEFT);
+		// }
+		// if (keycode == Keys.D) {
+		// startScreenQuake(Level.RIGHT);
+		// }
 		if (keycode == Keys.CONTROL_LEFT) {
 			level.player.tryHit();
 		}
