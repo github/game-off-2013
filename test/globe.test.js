c23bb725a8f0dba4d19beeb60a3511348af06117
hgcummings <harry@xlr3.com>
2013-11-19 23:57:21 +0000

    WIP

---
+++ test/globe.test.js
@@ -0,0 +1,18 @@
+define(function (require) {
+    'use strict';
+
+    var globe;
+
+    beforeEach(function() {
+        globe = require('globe');
+
+    });
+
+    describe('render', function() {
+//        it ('should render icecaps', function() {
+//            globe.render();
+//
+//
+//        });
+    })
+});
