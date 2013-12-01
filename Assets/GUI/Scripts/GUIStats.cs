e0ed19e6cbc3ff1209e9501f0f5b92cf4247336a
Hylinn Taggart <hylinn.taggart+git@gmail.com>
2013-12-01 00:16:27 -0500

    Added stats

--- Assets/GUI/Scripts/GUIStats.cs
+++ Assets/GUI/Scripts/GUIStats.cs
@@ -2,29 +2,17 @@
 using System.Collections;
 
 public class GUIStats : MonoBehaviour {
-	
-	//GUIStyle civilians;
-	//GUIStyle zombies;
-	//GUIStyle soldiers;
-	//GUIStyle elapsedTime;
-	
-	void Start() {
-		//civilians = new GUIStyle();
-		//zombies = new GUIStyle();
-		//soldiers = new GUIStyle();
-		//elapsedTime = new GUIStyle();
-	}
-	
+
 	void OnGUI () {
 		if (Time.timeScale != 0) {
-			if (GUI.Button(new Rect(10, 10, 55, 20), "Restart")) {
+			if (GUI.Button(new Rect(10, 56, 55, 20), "Restart")) {
 				Application.LoadLevel(1);	
 			}
+			
+			GUI.Label(new Rect(10, 0, 500, 20), "Elapsed Time (seconds): " + Time.timeSinceLevelLoad);
+			GUI.Label(new Rect(10, 12, 500, 20), "Civilians: " + GameObject.FindGameObjectsWithTag("Civilian").Length);
+			GUI.Label(new Rect(10, 24, 500, 20), "Zombies: " + GameObject.FindGameObjectsWithTag("Zombie").Length);
+			GUI.Label(new Rect(10, 36, 500, 20), "Soldiers:  " + GameObject.FindGameObjectsWithTag("Soldier").Length);
 		}
-		//GUI.Label(new Rect(0, 36, 100, 20), "Elapsed Time: ", elapsedTime);
-		//GUI.Label(new Rect(0, 0,  100, 20), "Civilians: ", civilians);
-		//GUI.Label(new Rect(0, 12, 100, 20), "Zombies: ", soldiers);
-		//GUI.Label(new Rect(0, 24, 100, 20), "Soldiers: ", zombies);
-		
 	}
 }
