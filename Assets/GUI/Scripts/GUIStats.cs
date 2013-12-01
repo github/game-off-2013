4e1c85761ac6b3ad55602daf62b37e559f76323b
Hylinn Taggart <hylinn.taggart+git@gmail.com>
2013-11-30 23:57:50 -0500

    Added Reset

--- Assets/GUI/Scripts/GUIStats.cs
+++ Assets/GUI/Scripts/GUIStats.cs
@@ -16,6 +16,11 @@ public class GUIStats : MonoBehaviour {
 	}
 	
 	void OnGUI () {
+		if (Time.timeScale != 0) {
+			if (GUI.Button(new Rect(10, 10, 55, 20), "Restart")) {
+				Application.LoadLevel(1);	
+			}
+		}
 		//GUI.Label(new Rect(0, 36, 100, 20), "Elapsed Time: ", elapsedTime);
 		//GUI.Label(new Rect(0, 0,  100, 20), "Civilians: ", civilians);
 		//GUI.Label(new Rect(0, 12, 100, 20), "Zombies: ", soldiers);
