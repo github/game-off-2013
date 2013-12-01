a594c067c6b8ee267dff376b6afbe4c6ebb9db33
Hylinn Taggart <hylinn.taggart+git@gmail.com>
2013-12-01 01:25:10 -0500

    Added structure for ending screens

---
+++ Assets/GUI/Scripts/EndGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+	
+	void Awake() {
+		//GameObject.FindGameObjectWithTag("Defeat").SetActive(false);
+		//GameObject.FindGameObjectWithTag("Victory").SetActive(false);
+	}
+	
+	void OnGUI() {
+		if (Time.timeScale != 0) {
+			if (GameObject.FindGameObjectsWithTag("Zombie").Length == 0 && Time.timeSinceLevelLoad > 1.0f) {
+				//GameObject.FindGameObjectWithTag("Defeat").SetActive(true);
+			}
+			else if (GameObject.FindGameObjectsWithTag("Civilian").Length == 0) {
+				//GameObject.FindGameObjectWithTag("Victory").SetActive(true);
+			}
+		}
+	}
+}
